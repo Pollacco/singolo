6d6c6b91722fa602bbb5ceba58c5b4182d59d617
Andrey <andrey.sviontek@gmail.com>
2020-03-15 22:06:33 +0500

    feat: hide phones display

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const menu = document.querySelector('.navigation');
 const tags = document.querySelector('.tags');
 const images = document.querySelector('.layout-4-column');
+const sliderPhones = document.querySelector('.slider__items');
 
 const randomaizer = () => parseInt(Math.ceil(Math.random() * 100));
 const shuffleImages = () => {
@@ -11,7 +12,6 @@ const shuffleImages = () => {
 menu.addEventListener('click', (event) => {
     menu.querySelectorAll('li').forEach(elem => elem.classList.remove('active'));
     event.target.classList.add('active');
-    console.log(event.target);
 });
 
 tags.addEventListener('click', (event) => {
@@ -23,4 +23,9 @@ tags.addEventListener('click', (event) => {
 images.addEventListener('click', (event) => {
     images.querySelectorAll('img').forEach(elem => elem.classList.remove('img-selected'));
     event.target.classList.add('img-selected');    
+});
+
+sliderPhones.addEventListener('click', (event) => {
+    sliderPhones.querySelectorAll('img').forEach(elem => elem.classList.remove('hide'));
+    event.target.classList.add('hide');
 });
